c1f9bd8f220623bbcd144a3700c116fd43f9db54
leeboyin <leeboyin945@gmail.com>
2021-01-23 23:47:26 +0800

    install firebase analytics

--- src/main.js
+++ src/main.js
@@ -9,6 +9,13 @@ import Vue from 'vue';
 import router from '@/router';
 // import store from '@/store';
 
+// firebase
+import firebaseApp from 'firebase/app';
+import 'firebase/analytics';
+import firebaseConfig from '../firebase.config';
+firebaseApp.initializeApp(firebaseConfig);
+firebaseApp.analytics();
+
 import VueLazyload from 'vue-lazyload';
 
 Vue.use(VueLazyload, {
